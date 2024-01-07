391831e078ade726e8b8555d4278cad836792596
Ramón Maldonado <monterdi@gmail.com>
2024-01-07 19:47:11 +0100

    feat: diptongos como vocales seguidas sin comprobar si realmente es diptongo o hiato.

--- silabeador.py
+++ silabeador.py
@@ -1,4 +1,17 @@
-vowels = ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú', 'ü']
+open_vowels = ['a', 'e', 'o', 'á', 'é', 'í', 'ó', 'ú']
+closed_vowels = [ 'i', 'u', 'ü']
+vowels = open_vowels + closed_vowels
 
 def vowels_groups(word):
-    return list(filter(lambda l: l in vowels, word))
+    # return list(filter(lambda l: l in vowels, word))
+    groups = []
+    group = ''
+    for letter in word:
+        if letter in vowels:
+            group += letter
+        else:
+            groups.append(group)
+            group = ''
+    
+    groups.append(group)
+    return list(filter(lambda g: g != '', groups))
--- tests/test_silabeador.py
+++ tests/test_silabeador.py
@@ -3,3 +3,10 @@ from silabeador import vowels_groups
 def test_grupos_vocalicos_basicos():
     assert vowels_groups('constante') == ['o', 'a', 'e']
     assert vowels_groups('psicólogo') == ['i', 'ó', 'o', 'o']
+
+def test_grupos_vocalicos_diptongos_triptongos():
+    assert vowels_groups('caucho') == ['au', 'o']
+    assert vowels_groups('cigüeña') == ['i', 'üe', 'a']
+
+
+    
